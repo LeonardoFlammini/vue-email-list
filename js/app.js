b80b240b749e5ec8cdc06fbdab562064863ba985
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-10-06 16:46:14 +0200

    [done]

--- js/app.js
+++ js/app.js
@@ -3,10 +3,27 @@ const {createApp} = Vue;
 createApp({
   data(){
     return{
-
+      apiMail: "https://flynn.boolean.careers/exercises/api/random/mail",
+      mailString: "",
+      mailArray: [],
+      items: 10
     }
   },
   methods:{
-
+    getApi(){
+      for(let i = 0; i < this.items; i++){
+        axios.get(this.apiMail)
+                .then((response)=>{
+                 this.mailString = response.data.response;
+                 this.mailArray.push(this.mailString);
+                })
+      }
+      
+    }
+  },
+  mounted(){
+    this.getApi();
+    console.log(this.mailArray);
+    
   }
-}).mount("#app")s
+}).mount("#app");
